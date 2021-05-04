c3818d9c2a305bbe80d2da8a5ccff57044785a28
LucasDias2512 <projetistalucasdias@gmail.com>
2021-05-03 21:24:45 -0300

    segundo commit

--- AulaCsharp/AulaCsharp/Program.cs
+++ AulaCsharp/AulaCsharp/Program.cs
@@ -6,7 +6,7 @@ namespace AulaCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Lucas Dias!");
         }
     }
 }
