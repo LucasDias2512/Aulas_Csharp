2e8108f117527ebb0ea205c3b27d86a9e97c5b75
LucasDias2512 <projetistalucasdias@gmail.com>
2021-05-03 21:13:51 -0300

    criando meu primeiro programa

---
+++ AulaCsharp/AulaCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AulaCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
